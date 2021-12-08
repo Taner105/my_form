660b00e69b761b0e949e24ca7302ad8f3ca14f29
Taner105 <taneraydogan59@gmail.com>
2021-12-08 14:37:09 +0300

    .

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,34 +1,65 @@
 import {useState} from "react"
 import "./Form.css"
 const Form = () => {
-    const [name, setName] = useState("")
-    const handlerName = () => {
-        setName({name})
-    }
+    const [values, setValues] = useState({
+    name:"",
+    surname:"",
+    email:"",
+    country:""
+    
+    });
+
+    const handlerValues = (e) => {
+        setValues({...values, [e.target.name]: e.target.value});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if(values.name=="" || values.surname=="" || values.email=="" || values.country==""){
+            
+           alert(`Tüm değerleri girdiğinizden emin olunuz`) 
+        }
+        else{
+            alert ( `
+        userName: ${values.name}
+        surName : ${values.surname}
+        email   : ${values.email}
+        country : ${values.country}`
+        )
+
+        }
+        
+    };
+       
     return (
         
        <div className="container">
+           
            <div className="container-down">
-                <form>
                
+                <form onSubmit={handleSubmit}>
+                    <h2>Enter İnformation</h2>
                     <div>
                         <label htmlFor="name"> Name</label><br />
-                    <   input onChange={handlerName} type="text" name="name" id="name" placeholder="Name" />
+                    <   input onChange={handlerValues} type="text" value={values.name} name="name" placeholder="Name" />
                     </div>
                     <div className="sur">    
                         <label htmlFor="surname">Surname</label><br />
-                        <input type="text" name="surname" id="surname" placeholder="Surname" />
+                        <input onChange={handlerValues} type="text" value={values.surname} name="surname" placeholder="Surname" />
                     </div>
                     <div className="ema">
                         <label htmlFor="email"> Email</label><br />
-                        <input type="email" name="email" id="email" placeholder="Email" />
+                        <input onChange={handlerValues} type="email" value={values.email} name="email" placeholder="Email" />
                     </div>
+
+
                     <label id="count" htmlFor="country">Choose a Country</label><br />
-                    <select name="country" id="country">
+                    <select value={values.country} onChange={handlerValues} name="country" id="country">
                         <option value="turkey">Turkey</option>
-                        <option value="turkey">Germany</option>
-                        <option value="turkey">USA</option>
-                        <option value="turkey">Hollanda</option>
+                        <option value="germany">Germany</option>
+                        <option value="usa">USA</option>
+                        <option value="hollanda">Hollanda</option>
                     </select><br />
                     <button type="submit">Submit</button>
                 </form>
